e749a4095484a314eb10a6bdb4554d77a242eabb
Garima Sharma <071.garima@gmail.com>
2026-08-22 02:39:24 +0530

    clean up
    clean up

--- backend/app/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Woodful Creations Backend Application Package
-"""
--- backend/docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "Starting application (database migrations run automatically on startup)..."
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000
